b3f0c5d64c2d5b7ab530a34c3f147d244b1e698b
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-10 16:07:44 -0300

    feito o layout e configurado as propriedades dos elementos

--- DesafioAppAlcoolEGasolina/DesafioAppAlcoolEGasolina/ViewController.swift
+++ DesafioAppAlcoolEGasolina/DesafioAppAlcoolEGasolina/ViewController.swift
@@ -11,12 +11,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var ethanolPriceLabel: UILabel!
+    
+    @IBOutlet weak var gasPriceLabel: UILabel!
+    
+    @IBOutlet weak var ethanolPriceTextField: UITextField!
+    
+    @IBOutlet weak var gasPriceTextField: UITextField!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configFontsEColors()
         
     }
+    
+    func configFontsEColors() {
+        view.backgroundColor = .systemIndigo
+        logoImageView.image = UIImage(named: "gas")
+        ethanolPriceLabel.text = "Preço Álcool"
+        gasPriceLabel.text = "Preço Gasolina"
+        ethanolPriceTextField.font = UIFont.boldSystemFont(ofSize: 18)
+        gasPriceTextField.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        gasPriceTextField.placeholder = "2,99"
+        ethanolPriceTextField.placeholder = "Ex: 1,99"
+        gasPriceTextField.textAlignment = .center
+        ethanolPriceTextField.textAlignment = .center
+        
+        gasPriceTextField.keyboardType = .decimalPad
+        ethanolPriceTextField.keyboardType = .decimalPad
+        
+        gasPriceTextField.delegate = self
+        ethanolPriceTextField.delegate = self
+        
+        calculateButton.setTitle("Calcular", for: .normal)
+        calculateButton.setTitleColor(.white, for: .normal)
+        calculateButton.backgroundColor = .blue
+        
+        resultLabel.text = "Digite os preços!"
+        resultLabel.textColor = .white
+        resultLabel.backgroundColor = .blue
+        
+        calculateButton.isEnabled = false
+        calculateButton.setTitleColor(.white.withAlphaComponent(0.4), for: .disabled)
+        }
+    
+    
+    
+    
+    
+    @IBAction func tappedCalculateButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    
 
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    //removido pq não tem necessidade de usar.
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if gasPriceTextField.text == "" || ethanolPriceTextField.text == ""{
+            calculateButton.isEnabled = false
+        } else {
+            calculateButton.isEnabled = true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 

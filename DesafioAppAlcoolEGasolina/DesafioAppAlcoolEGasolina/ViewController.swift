34372f35e515082da6969154f61a4bc82a058906
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-10 14:19:11 -0300

    criado o projeto desafioAppAlcoolEGasolina

---
+++ DesafioAppAlcoolEGasolina/DesafioAppAlcoolEGasolina/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DesafioAppAlcoolEGasolina
+//  Created by Ricardo Massaki on 10/03/23.
+//
+// Crie um repositório para o aplicativo. Durante todo o desenvolvimento, deverá ir criando commits e realizando o push. Ao final do projeto, todo o código deverá estar na branch da develop.
+// O aplicativo tem a finalidade de comparar os preços do alcool e a gasolina.
+// Se a diferença for maior que 70% deve utilizar gasolina, se não alcool.
+// Fazer tratativas para estado vazio, o usuário não pode relizar o calculo, sem ter os dois valores preenchidos.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

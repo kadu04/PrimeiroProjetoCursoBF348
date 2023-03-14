7d964498cac470d862e3eebd9b8789831d66488b
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-14 10:38:34 -0300

    criado navigation controller atraves do pushVIewController, e tela moda atraves do present, e finalizado o exercicio

--- exercicioNavegacao/exercicioNavegacao/ViewController.swift
+++ exercicioNavegacao/exercicioNavegacao/ViewController.swift
@@ -16,8 +16,18 @@ class ViewController: UIViewController {
 
     
     @IBAction func tappedTela02Button(_ sender: UIButton) {
-        performSegue(withIdentifier: "Tela02VC", sender: nil)
-
+        
+        let vc: Tela02VC? = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+                // O pushViewController ele exibe a tela da controladora.
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+        
+    }
+    
+    
+    @IBAction func tappedTela03Button(_ sender: UIButton) {
+        let vc: Tela03VC? = UIStoryboard(name: "Tela03VC", bundle: nil).instantiateViewController(withIdentifier: "Tela03VC") as? Tela03VC
+        present(vc ?? UIViewController(), animated: true)
+        
         }
     
     }

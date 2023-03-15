ffae6bcdcf5671d9a9188927f2a963d9502018f2
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-14 21:36:21 -0300

    criado o exercicio desafio TransitarObjetosEntreTelas

---
+++ TransitarObjetosEntreTelas/TransitarObjetosEntreTelas/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TransitarObjetosEntreTelas
+//
+//  Created by Ricardo Massaki on 14/03/23.
+
+// Desafio
+// Criar uma tela que contenha 4 textFields.
+// (nome, sobrenome, idade, profissão)
+// Criar um objeto chamado Pessoa.
+// Objetivo do desafio -> Transitar dados entre telas.
+// Leva as informações digitadas nos campos de texto através de um objeto para a tela 02.
+// Exibir os respectivos valores as propriedades do objeto nos textos das labels.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

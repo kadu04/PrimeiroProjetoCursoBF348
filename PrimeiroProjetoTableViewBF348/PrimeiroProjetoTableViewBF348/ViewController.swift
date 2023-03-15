391733b6044a1be35360c2757923870814196ed2
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-15 11:52:26 -0300

    criado o projeto TableView

---
+++ PrimeiroProjetoTableViewBF348/PrimeiroProjetoTableViewBF348/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoTableViewBF348
+//
+//  Created by Ricardo Massaki on 15/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+
+}
+

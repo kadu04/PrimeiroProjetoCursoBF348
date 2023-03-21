097c266eab58d8ca6e81e7804435a5c22d6b01c0
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-20 23:29:44 -0300

    foi criado o projeto com duas celulas diferentes, e finalizado o exercicioDesafio

---
+++ Desafio1TableViewAvancadoBF348/Desafio1TableViewAvancadoBF348/Model/Employee.swift
@@ -0,0 +1,19 @@
+//
+//  Employer.swift
+//  Desafio1TableViewAvancadoBF348
+//
+//  Created by Ricardo Massaki on 20/03/23.
+//
+
+import Foundation
+
+import UIKit
+
+struct Employee {
+    var name: String
+    var age: Int
+    var profession: String
+    var salary: String
+    var image: UIImage
+    var isEnableHeart: Bool
+}

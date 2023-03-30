8f4d0e271396e8438eeaee569c42fdad71623bf7
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-03-30 12:14:10 -0300

    criado demais maneiras para setar uma celula

--- TableViewCollectionViewAvancado/TableViewCollectionViewAvancado/ViewController.swift
+++ TableViewCollectionViewAvancado/TableViewCollectionViewAvancado/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var vehicleList: [Vehicle] = [
+        Vehicle(title: "Carro", list: ["car1","car2","car3","car4","car5","car6"]),
+        Vehicle(title: "Motocicleta", list: ["moto1","moto2","moto3","moto4","moto5","moto6"]),
+        Vehicle(title: "Avião", list: ["avião1","avião2","avião3","avião4","avião5","avião6"]),
+        Vehicle(title: "Caça", list: ["caça1","caça2","caça3","caça4","caça5","caça6"]),
+        Vehicle(title: "Trem", list: ["trem1","trem2","trem3","trem4","trem5","trem6"]),
+    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configTableView()
     }
+    
+    func configTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CarTableViewCell.nib(), forCellReuseIdentifier: CarTableViewCell.identifier)
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vehicleList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // abordagem com GUARD LET
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as? CarTableViewCell else { return UITableViewCell() }
+//        cell.setupCell(data: vehicleList[indexPath.row])
+//        return cell
+        
+        // Abordagem com IF LET
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as? CarTableViewCell {
+//            cell.setupCell(data: vehicleList[indexPath.row])
+//            return cell
+//        }
+//            return UITableViewCell()
+        
+        
+        // Abordagem com opcionalidade
+        let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as? CarTableViewCell
+        cell?.setupCell(data: vehicleList[indexPath.row])
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 386
+    }
+    
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    
 }
 
